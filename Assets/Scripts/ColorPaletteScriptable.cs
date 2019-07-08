6d50cd0051e755a7b5d3a47dd6acb8d8a4472106
nagedev <nagedevelop@gmail.com>
2019-07-08 20:51:27 +0300

    PaletteSwapping example

---
+++ Assets/Scripts/ColorPaletteScriptable.cs
@@ -0,0 +1,47 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using UnityEngine;
+using UnityEngine.Experimental.Rendering;
+
+namespace PaletteSwapping
+{
+    [CreateAssetMenu(fileName = "ColorPaletteScriptable", menuName = "ScriptableObjects/ColorPaletteScriptable")]
+    public class ColorPaletteScriptable : ScriptableObject
+    {
+        //array of colors that will be on palette
+        [SerializeField] private List<Color> colors;
+        
+        //generated palette texture. Allows to generate each palette only once for performance purposes
+        private Texture2D _texture;
+        
+        public Texture2D GetTexture()
+        {
+            //if texture has already been generated, return it
+            if (Application.isPlaying && _texture != null)
+                return _texture;
+            
+            //height of texture will be 1, and width is equal to amount of colors
+            Texture2D texture = new Texture2D(colors.Count, 1, TextureFormat.RGBA32, false);
+            //go through all colors in array
+            for (int i = 0; i < colors.Count; i++)
+            {
+                //set color of pixel to corresponding element of array
+                texture.SetPixel(i, 0, colors[i]);
+            }
+            
+            //set texture settings
+            texture.filterMode = FilterMode.Point;
+            texture.wrapMode = TextureWrapMode.Clamp;
+            
+            //apply changes
+            texture.Apply();
+            
+            //save generated texture
+            if (Application.isPlaying)
+                _texture = texture;
+            
+            return texture;
+        }
+    }
+}
---
+++ Assets/Scripts/UIColorPaletteComponent.cs
@@ -0,0 +1,47 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using PaletteSwapping;
+using UnityEngine;
+using UnityEngine.UI;
+
+namespace HexRL.UI
+{
+    [ExecuteInEditMode]
+    public class UIColorPaletteComponent : MonoBehaviour
+    {
+
+        //reference to palette scriptable object
+        [SerializeField] private ColorPaletteScriptable selectedPalette;
+        
+        //reference to ui image component
+        private Image _image;
+        
+        //shader property id
+        private static readonly int PaletteTex = Shader.PropertyToID("_PaletteTex");
+
+        private void Awake()
+        {
+            _image = GetComponent<Image>();
+
+            SetPalette(selectedPalette);
+        }
+
+        #if UNITY_EDITOR
+        private void Update()
+        {
+            if (Application.isPlaying)    //to save performance in play mode
+                return;
+            
+            SetPalette(selectedPalette);
+        }
+        #endif
+
+        public void SetPalette(ColorPaletteScriptable palette)
+        {
+            if (palette == null)
+                return;
+            
+            _image.material.SetTexture(PaletteTex, palette.GetTexture());
+        }
+    }
+}
